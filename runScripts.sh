ff70624b535954c4b8bbedc48975bb75dc3c1083
Oisín Creaner <creanero@users.noreply.github.com>
2021-05-25 16:04:15 +0100

    Dropped call to setEnv script to call root copy

--- runScripts.sh
+++ runScripts.sh
@@ -14,6 +14,6 @@ SCRIPT_ROOT=$(dirname "$(readlink -f "$0")")
 # Runs the configuration scripts in order 
 # Will want to comment these further - OC
 source $SCRIPT_ROOT/fermi-bashrc
-source $SCRIPT_ROOT/DataReadout/setEnvironment.sh
+source $SCRIPT_ROOT/setEnvironment.sh
 #source $SCRIPT_ROOT/DataReadout/ReadoutControls/startPulseServers.sh
 source $SCRIPT_ROOT/DataReadout/ChannelizerControls/startSeqFirmware.sh
