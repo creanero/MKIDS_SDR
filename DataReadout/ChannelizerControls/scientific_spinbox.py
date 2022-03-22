d014f462960c7fe20c24051bcf45e2cbfbc18bca
creanero <creanero@gmail.com>
2022-03-22 16:18:59 +0000

    fixed to include deca- and allow for undefined input.

--- DataReadout/ChannelizerControls/scientific_spinbox.py
+++ DataReadout/ChannelizerControls/scientific_spinbox.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import warnings
 from PyQt4 import QtGui
 import PyQt4.QtCore as QtC
 
@@ -10,8 +10,8 @@ class SIPrefixSpinbox(QtGui.QDoubleSpinBox):
     # Initial start from mguijarr via StackOverflow
     def __init__(self, *args):
         QtGui.QSpinBox.__init__(self, *args)
-        #
-        self.regex_string='(([+-]?\d+)[.]?(\d+)?[e]?[+-]?(\d+)?\s?([YZEPTGMkhdcmunpfazy])?)'
+        # complex regex string to recognise any possible number and the SI prefixes (including da as a separate case)
+        self.regex_string='(([+-]?\d+)[.]?(\d+)?[e]?[+-]?(\d+)?\s?([YZEPTGMkhdcmunpfazy]|da)?)'
         self.rx = QtC.QRegExp(self.regex_string)
         self.validator = QtGui.QRegExpValidator(self.rx, self)
 
@@ -35,52 +35,80 @@ class SIPrefixSpinbox(QtGui.QDoubleSpinBox):
 
 
 def SI_to_float(in_string):
-    si_value=in_string[:-1]
-    si_prefix=in_string[-1]
-    if si_prefix== 'Y':
-        value=float(si_value)*1e24
-    elif si_prefix== 'Z':
-        value=float(si_value)*1e21
-    elif si_prefix== 'E':
-        value=float(si_value)*1e18
-    elif si_prefix== 'P':
-        value=float(si_value)*1e15
-    elif si_prefix== 'T':
-        value=float(si_value)*1e12
-    elif si_prefix== 'G':
-        value=float(si_value)*1e9
-    elif si_prefix== 'M':
-        value=float(si_value)*1e6
-    elif si_prefix== 'k':
-        value=float(si_value)*1e3
-    elif si_prefix== 'h':
-        value=float(si_value)*1e2
-    elif si_prefix== 'da':
-        value=float(si_value)*1e1
-    elif si_prefix== 'd':
-        value=float(si_value)*1e-1
-    elif si_prefix== 'c':
-        value=float(si_value)*1e-2
-    elif si_prefix== 'm':
-        value=float(si_value)*1e-3
-    elif si_prefix== 'u':
-        value=float(si_value)*1e-6
-    elif si_prefix== 'μ':
-        value=float(si_value)*1e-6
-    elif si_prefix== 'n':
-        value=float(si_value)*1e-9
-    elif si_prefix== 'p':
-        value=float(si_value)*1e-12
-    elif si_prefix== 'f':
-        value=float(si_value)*1e-15
-    elif si_prefix== 'a':
-        value=float(si_value)*1e-18
-    elif si_prefix== 'z':
-        value=float(si_value)*1e-21
-    elif si_prefix== 'y':
-        value=float(si_value)*1e-24
-    else:
-        value = float(in_string)
+    '''
+    This function takes a string with an SI prefix at the end of it and returns the value of the quantity in the base
+    unit. For example, if given the string 20c it will return 0.2.  This is designed for use with the SIPrefixSpinBox
+    class which extends the PyQT4 QDoubleSpinBox class, which can strip the base unit (e.g. Hz) from the value string
+    before passing to this function.  If this is used elsewhere, then you need to handle that functionality yourself
+    '''
+
+    # parses the input string until it's possible to convert the leading characters into a float
+    i = 0
+
+    # sets default values to be returned if the next step fails.
+    si_value = 0.0
+    si_prefix = ''
+
+    while i < len(in_string):
+        try:
+            # extracts the leading characters and converts them to a float
+            si_value=float(in_string[:len(in_string)-i])
+            # extracts the trailing characters for evaluation as an SI prefix
+            si_prefix=in_string[len(in_string)-i:]
+            # breaks the loop if it gets this far (expected failure on the float conversion)
+            i=len(in_string)
+
+        except ValueError:
+            # increments the loop to split the input string one more character from the end.
+            i=i+1
+
+    # iterates over the SI prefixes
+    if si_prefix== 'Y': # Yotta = 1e24
+        value=si_value*1e24
+    elif si_prefix== 'Z': # Zetta = 1e21
+        value=si_value*1e21
+    elif si_prefix== 'E': # Exa = 1e18
+        value=si_value*1e18
+    elif si_prefix== 'P': # Peta = 1e15
+        value=si_value*1e15
+    elif si_prefix== 'T': # Tera = 1e12
+        value=si_value*1e12
+    elif si_prefix== 'G': # Giga = 1e9
+        value=si_value*1e9
+    elif si_prefix== 'M': # Mega = 1e6
+        value=si_value*1e6
+    elif si_prefix== 'k': # Kilo = 1e3
+        value=si_value*1e3
+    elif si_prefix== 'h': # Hecto = 1e2
+        value=si_value*1e2
+    elif si_prefix== 'da': # Deca = 1e1
+        value=si_value*1e1
+    elif si_prefix== 'd': # deci = 1e-1
+        value=si_value*1e-1
+    elif si_prefix== 'c': # centi = 1e-2
+        value=si_value*1e-2
+    elif si_prefix== 'm': # milli = 1e-3
+        value=si_value*1e-3
+    elif si_prefix== 'u': # micro (ascii-limited) = 1e-6
+        value=si_value*1e-6
+    elif si_prefix== 'mc': # micro (ascii-limited) = 1e-6
+        value=si_value*1e-6
+    elif si_prefix== 'μ': # micro = 1e-6
+        value=si_value*1e-6
+    elif si_prefix== 'n': # nano = 1e-9
+        value=si_value*1e-9
+    elif si_prefix== 'p': # pico = 1e-12
+        value=si_value*1e-12
+    elif si_prefix== 'f': # femto = 1e-15
+        value=si_value*1e-15
+    elif si_prefix== 'a': # atto = 1e-18
+        value=si_value*1e-18
+    elif si_prefix== 'z': # zepto = 1e-21
+        value=si_value*1e-21
+    elif si_prefix== 'y': # yocto = 1e-24
+        value=si_value*1e-24
+    else: # no or unrecognised prefix
+        value = si_value
     return value
 
 def SI_prefix_calculator(value):
